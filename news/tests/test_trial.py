427b90122cd1c869648efcc888d6d77d53389134
Zi0001 <WorckAcTop.218@gmail.com>
2024-08-03 19:20:09 +0300

    tests

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,59 +1,59 @@
-from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+# from django.contrib.auth import get_user_model
+# from django.test import Client, TestCase
 
-from news.models import News
-
-
-# Получаем модель пользователя.
-User = get_user_model()
+# from news.models import News
 
 
-# class Test(TestCase):
-#     def test_example_success(self):
-#         self.assertTrue(True)
+# # Получаем модель пользователя.
+# User = get_user_model()
 
 
-# class YetAnotherTest(TestCase):
-#     def test_example_fails(self):
-#         self.assertTrue(False)
-
-# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
-class TestNews(TestCase):
-
-    # Все нужные переменные сохраняем в атрибуты класса.
-    TITLE = 'Заголовок новости'
-    TEXT = 'Тестовый текст'
-
-    # В методе класса setUpTestData создаём тестовые объекты.
-    # Оборачиваем метод соответствующим декоратором.    
-    @classmethod
-    def setUpTestData(cls):
-
-        # Создаём пользователя.
-        cls.user = User.objects.create(username='testUser')
-        # Создаём объект клиента.
-        cls.user_client = Client()
-        # "Логинимся" в клиенте при помощи метода force_login().        
-        cls.user_client.force_login(cls.user)
-        # Теперь через этот клиент можно отправлять запросы
-        # от имени пользователя с логином "testUser".
+# # class Test(TestCase):
+# #     def test_example_success(self):
+# #         self.assertTrue(True)
 
 
-        # Стандартным методом Django ORM create() создаём объект класса.
-        # Присваиваем объект атрибуту класса: назовём его news.
-        cls.news = News.objects.create(
-            title = TITLE,
-            text= TEXT ,
-        )
+# # class YetAnotherTest(TestCase):
+# #     def test_example_fails(self):
+# #         self.assertTrue(False)
 
-    # Проверим, что объект действительно было создан.
-    def test_successful_creation(self):
-        # При помощи обычного ORM-метода посчитаем количество записей в базе.
-        news_count = News.objects.count()
-        # Сравним полученное число с единицей.
-        self.assertEqual(news_count, 1)
+# # Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
+# class TestNews(TestCase):
 
-    def test_title(self):
-        # Чтобы проверить равенство с константой -
-        # обращаемся к ней через self, а не через cls:
-        self.assertEqual(self.news.title, self.TITLE)   
+#     # Все нужные переменные сохраняем в атрибуты класса.
+#     TITLE = 'Заголовок новости'
+#     TEXT = 'Тестовый текст'
+
+#     # В методе класса setUpTestData создаём тестовые объекты.
+#     # Оборачиваем метод соответствующим декоратором.    
+#     @classmethod
+#     def setUpTestData(cls):
+
+#         # Создаём пользователя.
+#         cls.user = User.objects.create(username='testUser')
+#         # Создаём объект клиента.
+#         cls.user_client = Client()
+#         # "Логинимся" в клиенте при помощи метода force_login().        
+#         cls.user_client.force_login(cls.user)
+#         # Теперь через этот клиент можно отправлять запросы
+#         # от имени пользователя с логином "testUser".
+
+
+#         # Стандартным методом Django ORM create() создаём объект класса.
+#         # Присваиваем объект атрибуту класса: назовём его news.
+#         cls.news = News.objects.create(
+#             title = TITLE,
+#             text= TEXT ,
+#         )
+
+#     # Проверим, что объект действительно было создан.
+#     def test_successful_creation(self):
+#         # При помощи обычного ORM-метода посчитаем количество записей в базе.
+#         news_count = News.objects.count()
+#         # Сравним полученное число с единицей.
+#         self.assertEqual(news_count, 1)
+
+#     def test_title(self):
+#         # Чтобы проверить равенство с константой -
+#         # обращаемся к ней через self, а не через cls:
+#         self.assertEqual(self.news.title, self.TITLE)   
---
+++ t.py
@@ -0,0 +1,7 @@
+i = [1,2,3,4,5,6,7,8,9]
+
+i2 = sorted(i, reverse=True)
+
+print(f"i = {i}: i2 = {i2}")
+
+print(i == i2)
